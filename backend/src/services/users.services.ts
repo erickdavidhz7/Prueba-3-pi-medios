655cfe3a523fbff79aeec3ab0bea125049878800
Erick Henríquez <erickdavid7@hotmail.com>
2024-05-21 14:23:18 -0500

    added services in order to create users, get all users, find user by id, update user and change user's role

---
+++ backend/src/services/users.services.ts
@@ -0,0 +1,63 @@
+import UserI from '../interfaces/user.interface'
+import { Users } from '../models/user.model'
+
+const userServices = {
+  createUsers: async (user: UserI) => {
+    try {
+      if (!user.document || !user.last_name || !user.name) {
+        throw new Error('Missing Data')
+      }
+      const newUser = await Users.create({
+        document: user.document,
+        last_name: user.last_name,
+        name: user.name,
+      })
+      return newUser
+    } catch (error) {
+      console.log(error)
+      throw new Error(error as string)
+    }
+  },
+  getAllUsers: async () => {
+    try {
+      const users = await Users.findAll()
+      return users
+    } catch (error) {
+      throw new Error('Unable to search users')
+    }
+  },
+  findUserById: async (id: string) => {
+    try {
+      const userId = await Users.findOne({
+        where: {
+          id,
+        },
+      })
+
+      return userId
+    } catch (error) {
+      throw new Error('Error searching the user')
+    }
+  },
+
+  updateUser: async (id: string, data: Partial<UserI>) => {
+    try {
+      const userToUpdate = await Users.findByPk(id)
+      if (!userToUpdate) throw new Error('Error searching the user')
+      userToUpdate.update(data)
+    } catch (error) {
+      throw new Error('Error updating the user')
+    }
+  },
+  changeUserRole: async (id: string, role: string) => {
+    try {
+      const userToUpdate = await Users.findByPk(id)
+      if (!userToUpdate) throw new Error('Error searching the user')
+      userToUpdate.update({ roles_id: role })
+    } catch (error) {
+      throw new Error(`Error changing the user's role`)
+    }
+  },
+}
+
+export default userServices
