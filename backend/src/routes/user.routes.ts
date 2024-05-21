d64a0e76a91bbe420e87136dd71b37c4e916594a
Erick Henríquez <erickdavid7@hotmail.com>
2024-05-21 16:52:42 -0500

    included post route to create users

--- backend/src/routes/user.routes.ts
+++ backend/src/routes/user.routes.ts
@@ -4,6 +4,7 @@ import express from "express";
 const router = express.Router()
 
 router.get('/', UsersControllers.getAllUsers)
+router.post('/', UsersControllers.createUser)
 router.get('/:document', UsersControllers.findUserByDocument)
 router.patch('/:id', UsersControllers.updateUser)
 router.patch('/role/:id', UsersControllers.changeUserRole)
