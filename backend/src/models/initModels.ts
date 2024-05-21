b8d14b51b95622deb79a2a91e87ad1f8b0428a57
Erick Henríquez <erickdavid7@hotmail.com>
2024-05-21 13:44:15 -0500

    added a json file as a seed

--- backend/src/models/initModels.ts
+++ backend/src/models/initModels.ts
@@ -1,12 +1,10 @@
-import { Products } from "./product.model"
-import { Roles } from "./role.model"
-import { Sales } from "./sale.model"
-import { Users } from "./user.model"
-
+import { Products } from './product.model'
+import { Roles } from './role.model'
+import { Sales } from './sale.model'
+import { Users } from './user.model'
 
 export const initModels = () => {
-  Roles.hasMany(Users, {foreignKey: 'roles_id', sourceKey: 'id'})
-  Users.hasMany(Sales, {foreignKey: 'users_id', sourceKey: 'id'})
-  Products.hasMany(Sales, {foreignKey: 'products_id', sourceKey: 'id'})
-  
+  Roles.hasMany(Users, { foreignKey: 'roles_id', sourceKey: 'id' })
+  Users.hasMany(Sales, { foreignKey: 'users_id', sourceKey: 'id' })
+  Products.hasMany(Sales, { foreignKey: 'products_id', sourceKey: 'id' })
 }
